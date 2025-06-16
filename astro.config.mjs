2712069f2a44d57f2cdeca38c40f64440f2de00d
Kathiravan K <capgemini.kathiravan@mercedes-benz.com>
2025-06-16 13:17:17 +0530

    Added eslint

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,20 @@
+import vue from '@astrojs/vue'
+
+import tailwindcss from '@tailwindcss/vite'
+
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import vue from '@astrojs/vue';
-import viteVue from "@vitejs/plugin-vue";
-
-
-import tailwindcss from '@tailwindcss/vite';
-
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     vue({
       appEntrypoint: '/src/pages/_vueApp',
-      devtools: true
-    })
+      devtools: true,
+    }),
   ],
 
   vite: {
-    plugins: [tailwindcss()]
-  }
-});
+    plugins: [tailwindcss()],
+  },
+})
--- src/pages/_vueApp.ts
+++ src/pages/_vueApp.ts
@@ -2,9 +2,9 @@ import type { App } from 'vue';
 import PrimeVue from 'primevue/config';
 import Aura from '@primeuix/themes/aura';
 // import Material from '@primeuix/themes/material';
+import '@/styles/global.css'
 import 'primeicons/primeicons.css'
 import '@/styles/main.scss'
-
 
 export default (app: App) => {
     app.use(PrimeVue, {

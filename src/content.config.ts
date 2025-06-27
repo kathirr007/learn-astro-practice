be2aaf80c95e3d166dd87837b156544abd5783e6
Kathirr007 <kathirr007@gmail.com>
2025-06-27 12:21:32 +0530

    remove previous lessson files

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,18 +1,18 @@
-import { file } from "astro/loaders";
-import { defineCollection, z } from "astro:content";
+import { defineCollection, z } from 'astro:content'
+import { file } from 'astro/loaders'
 
 export const collections = {
   team: defineCollection({
-    loader: file("src/data/team.json"),
+    loader: file('src/data/team.json'),
     schema: z.object({
       name: z.string(),
       role: z.string(),
       email: z.string().email(),
       department: z.enum([
-        "Engineering",
-        "Software Development",
-        "Product Design",
+        'Engineering',
+        'Software Development',
+        'Product Design',
       ]),
     }),
   }),
-};
+}
